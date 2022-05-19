0d457af3d7fd1c7a82693a50bda6dced0df49a6b
bisrat <bisrat.amtataw@gmail.com>
2022-05-19 15:28:42 -0400

    env

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,24 +8,20 @@ function Footer(){
             <MDBFooter className='bg-dark text-center text-white footer' bgColor='#00695c'>
                 <MDBContainer className='p-4 pb-0' bgColor='#00695c'>
                 <section >
-
-            <a className='btn  btn-outline-light m-1 linkIn'  href='https://www.linkedin.com/in/bisrat-menberu/' role='button' style={{ backgroundColor: '#00695c'}}> LinkedIn
-            {/* <img src={LinkedFooter} alt='name'/> */}
+                    <a className='btn  btn-outline-light m-1 linkIn'  href='https://www.linkedin.com/in/bisrat-menberu/' role='button' style={{ backgroundColor: '#00695c'}}> LinkedIn
             <MDBIcon fab icon='linkedin-in' />
-            </a> 
-
-            <a className='btn btn-outline-light m-1 linkIn' href='https://github.com/BISrat3'  role='button' style={{ backgroundColor: '#00695c'}}>GitHub
-            {/* <MDBIcon fab icon='github' /> */}
-            </a>
-            </section>
+                    </a> 
+                    <a className='btn btn-outline-light m-1 linkIn' href='https://github.com/BISrat3'  role='button' style={{ backgroundColor: '#00695c'}}>GitHub
+                    </a>
+                </section>
             </MDBContainer >
             <MDBContainer fluid>
-        <div className='text-center p-3' style={{ backgroundColor: '#00695c' }}>Copyright:  
-            &copy; {new Date().getFullYear()}  Powered By sportmonks API
-            <a className='text-white foot Link' href='https://www.linkedin.com/in/bisrat-menberu/'>. All Rights Reserved.
-            By Bisrat Amtataw
-            </a>
-        </div>
+                <div className='text-center p-3' style={{ backgroundColor: '#00695c' }}>Copyright:  
+                    &copy; {new Date().getFullYear()}  Powered By sportmonks API
+                    <a className='text-white foot Link' href='https://www.linkedin.com/in/bisrat-menberu/'>. All Rights Reserved.
+                    By Bisrat Amtataw
+                    </a>
+                </div>
             </MDBContainer>
             </MDBFooter>
         </div>
